81486fa99bf9dfff270b9472604803662b923fb8
Kommai <dev@kommai.app>
2022-03-30 09:17:25 +0900

    Added __callStatic

--- src/Proxy.php
+++ src/Proxy.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kommai\TestKit;
 
+use LogicException;
 use ReflectionClass;
 use RuntimeException;
 use Throwable;
@@ -11,10 +12,16 @@ use Throwable;
 class Proxy
 {
     private object $object;
+    private static string $class;
 
-    public function __construct(object $object)
+    public function __construct(object|string $objectOrClass)
     {
-        $this->object = $object;
+        if (is_object($objectOrClass)) {
+            $this->object = $objectOrClass;
+        }
+        if (is_string($objectOrClass)) {
+            self::$class = $objectOrClass;
+        }
     }
 
     public function __get(string $name): mixed
@@ -31,9 +38,26 @@ class Proxy
     public function __call(string $name, array $arguments): mixed
     {
         try {
+            if (!isset($this->object)) {
+                throw new LogicException(sprintf('"%s" method cannot be called in static context', $name));
+            }
             $reflectionMethod = (new ReflectionClass($this->object))->getMethod($name);
             $reflectionMethod->setAccessible(true);
             return $reflectionMethod->invokeArgs($this->object, $arguments);
+        } catch (Throwable $thrown) {
+            throw new RuntimeException(sprintf('Failed to call "%s" method', $name), 0, $thrown);
+        }
+    }
+
+    public static function __callStatic(string $name, array $arguments): mixed
+    {
+        try {
+            if (!isset(self::$class)) {
+                throw new LogicException(sprintf('"%s" method cannot be called in object context', $name));
+            }
+            $reflectionMethod = (new ReflectionClass(self::$class))->getMethod($name);
+            $reflectionMethod->setAccessible(true);
+            return $reflectionMethod->invokeArgs(null, $arguments);
         } catch (Throwable $thrown) {
             throw new RuntimeException(sprintf('Failed to call "%s" method', $name), 0, $thrown);
         }
